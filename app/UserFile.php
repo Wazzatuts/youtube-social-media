00ef9b88fe77910aaa7ab58e88ccbf26b83ef201
Wayne Fellows <superluigi007@gmail.com>
2020-10-11 20:18:13 +0100

    Lesson 24 Setting relationships user and images

--- app/UserFile.php
+++ app/UserFile.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class UserFile extends Model
 {
-    //
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function toArray()
+    {
+        return [
+            'id' => $this->id,
+            'file_path' => $this->file_name
+        ];
+    }
 }
